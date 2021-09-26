62052128ad3015d4ca0beda29aeb4bdf652f533d
Tâm BE <tam.cl@tradingfoe.com>
2021-09-27 02:05:56 +0700

    landing page

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,7 +1,24 @@
 #
-class DashboardsController < ApplicationController
+class DashboardsController < AuthController
   def index
-    @total_users = User.count
+    @total_users = total_users_count
+    @total_links = total_links_count
+    @top_clicked = top_clicked
+
     render 'dashboards/index'
+  end
+
+  private
+
+  def total_users_count
+    User.count
+  end
+
+  def total_links_count
+    Link.count
+  end
+
+  def top_clicked
+    Link.top_clicked(current_user.id)
   end
 end
